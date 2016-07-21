54d09bd6025f82cc153ffbdc8e61795c3df554a1
Kristjan Gannon <gannonk08@gmail.com>
2016-07-21 11:10:38 -0600

    functional pixel art maker

---
+++ src/main.js
@@ -0,0 +1,33 @@
+$(function() {
+    'use strict';
+    var brush
+    var currentColor
+
+    function newCanvas() {
+        var pixel;
+        var canvas = $('#canvas')
+        for (var i = 0; i < 2223; i++) {
+            canvas.append($('<div class="pixel"></div>'));
+        }
+    }
+    newCanvas();
+
+    function coloredPixel() {
+        $('.pixel').mousedown(function() {
+            console.log("clicked pixel");
+            $(this).css('background-color', brush);
+        })
+    }
+    coloredPixel();
+
+    function colorSelector() {
+        $('#pallete > div').mousedown(function() {
+            brush = $(this).css("background-color")
+            console.log(brush);
+            $('html,body').css('cursor', 'crosshair');
+            $('#currentColor').css('background-color', brush)
+        })
+    }
+    colorSelector();
+
+}());
